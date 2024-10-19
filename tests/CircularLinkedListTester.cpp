64f0b2e8c70c3e8f84acbcb6cc5ad43a06de1b19
Willem Van Zwol <wvanzwol27@my.whitworth.edu>
2024-10-18 19:14:47 -0700

    pushpop method tests added

--- tests/CircularLinkedListTester.cpp
+++ tests/CircularLinkedListTester.cpp
@@ -8,6 +8,8 @@ int main(int argc, char **argv) {
     return RUN_ALL_TESTS();
 }
 
+// TODO: once you have the iterator setup use it to prove lists have circularity (both directions)
+
 TEST(PopBack, ThreeItems) {
     CS273::CircLinkedList<std::string> myList;
 	myList.push_back("item1");
@@ -17,7 +19,6 @@ TEST(PopBack, ThreeItems) {
     EXPECT_EQ(myList.size(), 2);
     EXPECT_EQ(myList.front(), "item1");
     EXPECT_EQ(myList.back(), "item2");
-    // TODO: once you have the iterator setup iterate through to make sure all works
 }
 
 TEST(PopBack, OneItem) {
@@ -43,4 +44,61 @@ TEST(PopFront, ThreeItems) {
     EXPECT_EQ(myList.size(), 2);
     EXPECT_EQ(myList.front(), "item2");
     EXPECT_EQ(myList.back(), "item3");
+}
+
+TEST(PopFront, OneItem) {
+    CS273::CircLinkedList<std::string> myList;
+	myList.push_back("item1");
+    myList.pop_front();
+    EXPECT_EQ(myList.empty(), 1);
+}
+
+TEST(PopFront, EmptyList) {
+    CS273::CircLinkedList<std::string> myList;
+    EXPECT_NO_THROW(myList.pop_front());
+}
+
+TEST(PushFront, ThreeItems) {
+    CS273::CircLinkedList<std::string> myList;
+    myList.push_back("item1");
+    myList.push_back("item2");
+    myList.push_back("item3");
+
+    myList.push_front("newItem");
+
+    EXPECT_EQ(myList.size(), 4);
+    EXPECT_EQ(myList.front(), "newItem");
+    EXPECT_EQ(myList.back(), "item3");
+}
+
+TEST(PushFront, MultiplePushes) {
+    CS273::CircLinkedList<std::string> myList;
+    myList.push_front("item1");
+    myList.push_front("item2");
+    myList.push_front("item3");
+
+    EXPECT_EQ(myList.size(), 3);
+    EXPECT_EQ(myList.front(), "item3");
+    EXPECT_EQ(myList.back(), "item1");
+}
+
+TEST(PushFront, OneItem) {
+    CS273::CircLinkedList<std::string> myList;
+    myList.push_back("item1");
+
+    myList.push_front("newItem");
+
+    EXPECT_EQ(myList.size(), 2);
+    EXPECT_EQ(myList.front(), "newItem");
+    EXPECT_EQ(myList.back(), "item1");
+}
+
+
+TEST(PushFront, EmptyList) {
+    CS273::CircLinkedList<std::string> myList;
+    myList.push_front("item1");
+
+    EXPECT_EQ(myList.size(), 1);
+    EXPECT_EQ(myList.front(), "item1");
+    EXPECT_EQ(myList.back(), "item1");
 }
